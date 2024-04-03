e6f4193b8bf8d18912c1fc1fcb5b16cbe6f3cc16
condor254 <106682742+condor254@users.noreply.github.com>
2024-04-03 10:34:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ module.exports = {
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY  || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "public",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
   LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
 
 
